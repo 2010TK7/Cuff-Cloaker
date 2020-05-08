8dffad4fe66d1fa5e65fa7a8046d17013df1b819
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-05-08 17:28:38 +0800

    3

--- Tarrested/base.lua
+++ Tarrested/base.lua
@@ -1,3 +1,3 @@
-local module = DorHUDMod:new("Tarrested", {name = "Cuff Cloaker", author = "TK7", description = "https://modworkshop.net/mod/27471", version = "2"})
+local module = DorHUDMod:new("Cuff Cloaker", {author = "TK7", description = "https://modworkshop.net/mod/27471", version = "3"})
 module:hook_post_require("lib/units/beings/player/playermovement", "playermovement")
 return module
